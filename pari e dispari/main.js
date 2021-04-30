f8321d902523f0e984288cf7d84ce190be9c0e34
domingo79 <andria140779@yahoo.it>
2021-04-30 15:52:14 +0200

    ripulito il codice

--- pari e dispari/main.js
+++ pari e dispari/main.js
@@ -1,43 +1,47 @@
-// Pari e Dispari:
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
+/**********************
+*   Pari e Dispari:
+***********************/
 
-
+//1. L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
 var scelta_utente = prompt('scegli vero o falso').toString().toLowerCase();
 var numero_utente = Number(prompt('scegli un numero da da 1 a 5'));
-var numero_random = generaNumero();
 
-function generaNumero() {
-    var numeroPC = Number(Math.floor(Math.random() * 5));
-    return numeroPC;
+//2. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+/**
+ * ### FUNZIONE --Genera numero random con parametri min(numero) e max(numero)--
+ * @param {numero minimo} min numero incluso
+ * @param {numero massimo} max numero incluso
+ * @returns {numero} ritorna un numero random tra min(num) e max (num)
+ */
+function RandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+var numero_random = RandomNumber(1, 5);
 
+//3. Sommiamo i due numeri
 var risultato = numero_utente + numero_random;
 
+//.4  Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+/**
+ * ### FUNZIONE Pari o dispari
+ * @param {number} numeri
+ * @returns valore booleano true o false
+ */
 function isEven(num) {
     if (num % 2 === 0) {
-
         return true;
     }
+    return false;
 }
-
-if (isEven(risultato)) {
-    risultato = 'pari';
+var checkWinLose = isEven(risultato);
+// 5. Dichiariamo chi ha vinto.
+if (checkWinLose === true && scelta_utente === 'vero') {
+    console.log('User  Win!!');
+} else if (checkWinLose === true && scelta_utente !== 'vero') {
+    console.log('User  Win!!');
 } else {
-    risultato = 'dispari'
+    console.log('PC Win!! Try again..');
 }
 
-if (risultato = 'pari' && scelta_utente === 'pari') {
-    console.log("hai vinto!");
-} else if (risultato = 'dispari' && scelta_utente === 'dispari') {
-    console.log("hai vinto!");
-} else {
-    console.log("Ritenta");
-}
 
-console.log(scelta_utente);
-console.log(numero_utente);
-console.log(numero_random);
 
